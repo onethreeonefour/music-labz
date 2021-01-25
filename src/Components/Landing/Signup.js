5578abb02a223ce492ed610c82b3cd8687add266
Terry Nguyen <plow-king@hotmail.com>
2021-01-25 13:35:24 +1100

    input update

--- src/Components/Landing/Signup.js
+++ src/Components/Landing/Signup.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Signup() {
-    return (
-        <div className="signup-container">
-            <h1>Interested in MusicLabz? Start a <span>30 day free</span> trial</h1>
-            <h3>Simply put your email to get started!</h3>
-            <label>Email</label>
-            <input></input>
-            <button className="teal-button">Submit</button>
-        </div>
-    )
+	return (
+		<div className="signup-container">
+			<h1>
+				Interested in MusicLabz? Start a <span>30 day free</span> trial
+			</h1>
+			<h3>Simply put your email to get started!</h3>
+			<label>Email</label>
+			<input placeholder="Put your email here to see the lastest news!"></input>
+			<button className="teal-button">Submit</button>
+		</div>
+	);
 }
 
-export default Signup
+export default Signup;
