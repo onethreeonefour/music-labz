ea0b81ca428f664b48da73fe656ec0a4f72ed926
Terry Nguyen <plow-king@hotmail.com>
2022-01-25 23:28:10 +1100

    5k update

--- src/Components/Landing/Signup.js
+++ src/Components/Landing/Signup.js
@@ -7,8 +7,8 @@ function Signup() {
         Interested in MusicLabz? Start a <span>30 day free</span> trial
       </h1>
       <h3 style={{ fontSize: "0.8rem" }}>This is actually a Spotify API application. Go Support Spotify!</h3>
-      <label>Email</label>
-      <input placeholder="Put your email here to see the latest news!"></input>
+      <label>Email Address</label>
+      <input placeholder="Enter your email address"></input>
       <button className="teal-button">Submit</button>
     </div>
   );
